fff52a21ab1fcebd82f24c443483fa5b71fb8f6f
Maksim Moskvin <me@moskvin.space>
2023-06-06 14:05:40 +0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -8,8 +8,10 @@ PACMAN="$1"
 $PACMAN update
 $PACMAN install -y docker
 service docker start
-usermod -a -G docker ec2-user
+usermod -a -G docker $USER
 chkconfig docker on
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker --version
 docker-compose version
+echo Don't forget to reboot the server to make the docker command work without sudo (๑˘︶˘๑)
