ea12f89dce53b5abc44fa5f7d93d20f00efb48c2
Maksim Moskvin <me@moskvin.space>
2023-06-06 13:16:27 +0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # docker-install.sh
 
-yum update
-yum -y install docker
+$PACMAN update
+$PACMAN -y install docker
 service docker start
 usermod -a -G docker ec2-user
 chkconfig docker on
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
