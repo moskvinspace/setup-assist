7b11c76bceccb963d6aea34d7e25d200d69016a7
Maksim Moskvin <me@moskvin.space>
2023-06-06 16:05:41 +0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -12,6 +12,6 @@ usermod -a -G docker $USER
 chkconfig docker on
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-docker --version;export statusdocker=$(echo $?)
-[$statusdocker -eq 0] && echo $'Dont forget to reboot the server to make the docker command work without sudo!'
+docker --version;statusdocker=$?;
+[ $statusdocker -eq 0 ] && echo $'Dont forget to reboot the server to make the docker command work without sudo!'
 docker-compose version
